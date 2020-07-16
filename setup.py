9e82503c01bdb5e31f88ca575c4d81adef4586de
Timothy John Morland <me@timothy-morland.com>
2020-07-16 12:30:55 -0400

    fixup! inital commit

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from distutils.core import setup
 
 setup(
     name="Game",
+    version="1.0.0",
+    description="My first game",
+    python_requires=">=3.7",
     install_requires=["pygame==1.9.6"],
     packages=["src"]
 )

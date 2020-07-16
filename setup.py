d5b7e7a591ca34b0ca530e6e1339f11520d04495
Timothy John Morland <me@timothy-morland.com>
2020-07-16 13:00:31 -0400

    bump pygame version to fix bug on osx

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     version="1.0.0",
     description="My first game",
     python_requires=">=3.7",
-    install_requires=["pygame==1.9.6"],
+    install_requires=["pygame==2.0.0.dev4"],
     packages=["game"]
 )
